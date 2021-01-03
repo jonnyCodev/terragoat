a083c05e428b9c4774ae0618cb984607466e6284
jonnyCodev <42693227+jonnyCodev@users.noreply.github.com>
2021-01-03 16:22:05 +0200

    Update db-app.tf

--- terraform/aws/db-app.tf
+++ terraform/aws/db-app.tf
@@ -2,6 +2,8 @@ resource "aws_db_instance" "default" {
   name                   = var.dbname
   engine                 = "mysql" 
   option_group_name      = aws_db_option_group.default.name
+  
+  
   parameter_group_name   = aws_db_parameter_group.default.name
   db_subnet_group_name   = aws_db_subnet_group.default.name
   vpc_security_group_ids = ["${aws_security_group.default.id}"]
