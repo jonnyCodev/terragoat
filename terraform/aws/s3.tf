648cc14088a23e604bd6100099ebff5f78908137
jonnyCodev <yonicodev@gmail.com>
2021-04-21 14:47:26 +0300

    a

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -4,8 +4,8 @@ resource "aws_s3_bucket" "data" {
   # bucket does not have access logs
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-data"
-  acl           = "public-read"
-  force_destroy = false
+  acl           = "private"
+  force_destroy = true
   tags = {
     Name        = "${local.resource_prefix.value}-data"
     Environment = local.resource_prefix.value
