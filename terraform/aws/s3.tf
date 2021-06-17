76b829def938d911545fd10708ef0857a58cdb66
jonnyCodev <42693227+jonnyCodev@users.noreply.github.com>
2021-06-17 15:32:24 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -33,6 +33,8 @@ resource "aws_s3_bucket" "financials" {
   
   bucket        = "${local.resource_prefix.value}-financials"
   acl           = "private"
+  
+  
   force_destroy = true
   tags = {
     Name        = "${local.resource_prefix.value}-financials"
