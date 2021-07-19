4ab875a34626dd561b3d3932ee336fb952a3a90b
jonnyCodev <42693227+jonnyCodev@users.noreply.github.com>
2021-07-19 10:54:16 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -1,14 +1,6 @@
 resource "aws_s3_bucket" "financials" {
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-  versioning {
-    enabled = true
-  }
+
+  
   # lala
   # bucket is not encrypted
   # bucket does not have access logs
