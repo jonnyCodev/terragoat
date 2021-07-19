daa107244d179874af7b5f64400be8f14a7f6c1d
jonnyCodev <42693227+jonnyCodev@users.noreply.github.com>
2021-07-19 10:00:34 +0300

    Update terraform/aws/s3.tf
    Co-authored-by: bridgecrew-dev[bot] <83754225+bridgecrew-dev[bot]@users.noreply.github.com>

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -15,7 +15,6 @@ resource "aws_s3_bucket" "financials" {
   # bucket does not have versioning
   
   bucket        = "${local.resource_prefix.value}-financials"
-  acl           = "public-read"
   force_destroy = true
   tags = {
     Name        = "${local.resource_prefix.value}-financials"
