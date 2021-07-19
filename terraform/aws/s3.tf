083026ecfe43a9adb19902bbb04c697c53ffa1fa
jonnyCodev <42693227+jonnyCodev@users.noreply.github.com>
2021-07-19 10:28:11 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -1,21 +1,11 @@
 resource "aws_s3_bucket" "financials" {
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-  versioning {
-    enabled = true
-  }
   # lala
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
   
   bucket        = "${local.resource_prefix.value}-financials"
-  acl           = "private"
+  acl           = "public-read"
   force_destroy = true
   tags = {
     Name        = "${local.resource_prefix.value}-financials"
